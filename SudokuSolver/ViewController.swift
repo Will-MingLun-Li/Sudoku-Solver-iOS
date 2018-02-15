a8742e363679714b4c9f3fca1744890ff882ef3d
Will Li <will.liatw@gmail.com>
2018-02-15 16:46:32 -0500

    Creating project

---
+++ SudokuSolver/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SudokuSolver
+//
+//  Created by Will Li on 2018-02-15.
+//  Copyright © 2018 Will Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
